16f309ebee6135331252008ec2d6580fcf93e5da
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-11-27 15:48:03 +0100

    added initial files to the front-end

--- sources/front-end/svelte.config.js
+++ sources/front-end/svelte.config.js
@@ -1,13 +1,28 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  compilerOptions: {
+    format: 'esm',
+    hydratable: true,
+  },
+  extensions: ['.svelte'],
+  kit: {
+    adapter: adapterNode({
+      precompress: true,
+    }),
+    // hydrate the <div id="macaroons-berlin"> element in src/app.html
+    target: '#schedule',
+    hydrate: true,
+    router: true,
+    ssr: true,
+    files: {
+      serviceWorker: 'src/service-worker/index.mjs',
+    },
+    serviceWorker: {
+      files: (filepath) => !/\.DS_STORE/.test(filepath),
+    },
+  },
 };
 
 export default config;
