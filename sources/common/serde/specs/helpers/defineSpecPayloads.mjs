0ea4f49aca97b00c56055c36179cbbae7e45b293
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-12-07 18:08:01 +0100

    extracted constructors to separate files

--- sources/common/serde/specs/helpers/defineSpecPayloads.mjs
+++ sources/common/serde/specs/helpers/defineSpecPayloads.mjs
@@ -1,66 +1,11 @@
-/* eslint-disable max-classes-per-file */
 import {
-  randomUUID,
-} from 'node:crypto';
+  constructRegistrationRequest,
+} from './constructRegistrationRequest.mjs';
 import {
-  Payload,
-} from '../../fbs/generated/mjs/schedule/payload.mjs';
-import {
-  COSEAlgorithms,
-} from '../../fbs/generated/mjs/schedule/c-o-s-e-algorithms.mjs';
-import {
-  PublicKeyCredentialType,
-} from '../../fbs/generated/mjs/schedule/public-key-credential-type.mjs';
-import {
-  AuthenticatorAttachment,
-} from '../../fbs/generated/mjs/schedule/authenticator-attachment.mjs';
-import {
-  ResidentKeyRequirement,
-} from '../../fbs/generated/mjs/schedule/resident-key-requirement.mjs';
-import {
-  AttestationConveyancePreference,
-} from '../../fbs/generated/mjs/schedule/attestation-conveyance-preference.mjs';
+  constructPublicKeyCredentialCreationOptions,
+} from './constructPublicKeyCredentialCreationOptions.mjs';
 
-const defineRegistrationRequestClass = () => ({
-  type: Payload.RegistrationRequest,
-});
-
-const definePublicKeyCredentialCreationOptionsClass = () => {
-  const publicKeyCredentialCreationOptions = {
-    type: Payload.PublicKeyCredentialCreationOptions,
-  };
-
-  publicKeyCredentialCreationOptions.challenge = Uint8Array.from(randomUUID());
-  publicKeyCredentialCreationOptions.rp = {
-    id: randomUUID(),
-    name: randomUUID(),
-  };
-  publicKeyCredentialCreationOptions.user = {
-    id: Uint8Array.from(randomUUID()),
-    name: randomUUID(),
-    displayName: randomUUID(),
-  };
-  publicKeyCredentialCreationOptions.pubKeyCredParams = [{
-    alg: COSEAlgorithms.ES256,
-    type: PublicKeyCredentialType.publicKey,
-  }];
-  publicKeyCredentialCreationOptions.authenticatorSelection = {
-    authenticatorAttachment: AuthenticatorAttachment.platform,
-    userVerification: ResidentKeyRequirement.required,
-  };
-  publicKeyCredentialCreationOptions.timeout = 60000;
-  publicKeyCredentialCreationOptions.attestation = AttestationConveyancePreference.direct;
-
-  return publicKeyCredentialCreationOptions;
-};
-
-export const defineSpecPayloads = () => {
-  const result = [];
-
-  result.push(
-    defineRegistrationRequestClass(),
-    definePublicKeyCredentialCreationOptionsClass(),
-  );
-
-  return result.slice();
-};
+export const defineSpecPayloads = () => [
+  constructRegistrationRequest(),
+  constructPublicKeyCredentialCreationOptions(),
+];
