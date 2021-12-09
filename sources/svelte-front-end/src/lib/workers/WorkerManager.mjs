00e880e69741fb2d4d11f97a8898eee18d540ff9
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-12-09 22:31:04 +0100

    okay, a lot of various fixes and additions.

--- sources/svelte-front-end/src/lib/workers/WorkerManager.mjs
+++ sources/svelte-front-end/src/lib/workers/WorkerManager.mjs
@@ -1,24 +1,25 @@
 import {
   Communicator,
 } from './Communicator/Communicator.mjs';
+import {
+  Serializer,
+} from './Serializer/Serializer.mjs';
 
 export class WorkerManager {
   constructor(communicatorConfig = null) {
     this.communicatorConfig = Object.freeze({ ...communicatorConfig });
     this.communicator = null;
+    this.serializer = null;
 
     console.log(`${this.constructor.name}.ctor`);
   }
 
   async start() {
+    this.serializer = new Serializer();
     this.communicator = new Communicator();
 
+    await this.serializer.start();
     await this.communicator.start(`${this.communicatorConfig.address}/`);
-
-    // window.postMessage({
-    //   type: 'register:start',
-    //   payload: {},
-    // });
 
     console.log(`${this.constructor.name}.start`);
   }
